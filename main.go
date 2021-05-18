db4d80ba71278f97b973ce203530abb1063d48db
Obed N Munoz <obed.n.munoz@gmail.com>
2021-05-17 22:54:26 -0500

    Initial commit
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/CodersSquad/dc-final/controller"
+	"github.com/CodersSquad/dc-final/scheduler"
+)
+
+func main() {
+	log.Println("Welcome to the Distributed and Parallel Image Processing System")
+
+	// Start Controller
+	go controller.Start()
+
+	// Start Scheduler
+	jobs := make(chan scheduler.Job)
+	go scheduler.Start(jobs)
+	// Send sample jobs
+	sampleJob := scheduler.Job{Address: "localhost:50051", RPCName: "hello"}
+
+	for {
+		sampleJob.RPCName = fmt.Sprintf("hello-%v", rand.Intn(10000))
+		jobs <- sampleJob
+		time.Sleep(time.Second * 5)
+	}
+	// API
+	// Here's where your API setup will be
+}
